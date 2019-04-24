b1d7357a749175de0e2280607fc15abd013af017
songjiang <songjiang@100tal.com>
2019-04-24 11:21:12 +0800

    hellomod

--- hellomod.go
+++ hellomod.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func Indexhandler(w http.ResponseWriter,r *http.Request)  {
+    fmt.Fprintln(w,"hello world")
+}
 
 func main() {
 	fmt.Println("hello,time99")
 	var val int = 10
 	fmt.Println(val)
+	http.HandleFunc("/",Indexhandler)
+    http.ListenAndServe("127.0.0.1",nil)
 }
